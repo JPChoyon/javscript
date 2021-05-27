2a4c20431be24e9355c6bb319a3355c6250034c0
JPChoyon <59580842+JPChoyon@users.noreply.github.com>
2021-05-27 23:35:01 +0600

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var name = prompt('enter your full name');
 var mobile = prompt('enter your mobile number');
 
-document.write("your full name "+ name);
+document.write("your full name "+ <b>name</b>);
 document.write("<br/> your mobile number "+ mobile);
 
