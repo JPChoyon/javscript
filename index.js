842d90ac503517eb37f37bcb0dd7c2a7635463ac
JPChoyon <59580842+JPChoyon@users.noreply.github.com>
2021-05-27 23:31:29 +0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,6 @@
+var name = prompt('enter your full name');
+var mobile = prompt('enter your mobile number');
+
+document.write("your full name "+ name);
+document.write("<br/> your mobile number "+ mobile);
+
