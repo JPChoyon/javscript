5112b19152d65276f44607c807a1a77472e64424
JPChoyon <59580842+JPChoyon@users.noreply.github.com>
2021-06-12 23:30:40 +0600

    Add files via upload

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-var name = prompt('enter your full name');
-var mobile = prompt('enter your mobile number');
-
-document.write("your full name "+ <b>name</b>);
-document.write("<br/> your mobile number "+ mobile);
-
+var myVar = document.querySelector("#img")
+function pic1(){
+    myVar.src="pic_bulboff.gif"
+};
+function pic2(){
+    myVar.src="pic_bulbon.gif"
+};
