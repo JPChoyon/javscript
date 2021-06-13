c9661775733395ded80d1e6b85846a208c686059
JPChoyon <59580842+JPChoyon@users.noreply.github.com>
2021-06-14 00:11:46 +0600

    Add files via upload

---
+++ class36 - dynamic style/index.js
@@ -0,0 +1,10 @@
+function add() {
+    var any = document.querySelector("span");
+    any.style.color = "blue";
+    any.style.fontSize= "25px";
+}
+function rem() {
+    var any = document.querySelector("span");
+    any.style.color = "black";
+    any.style.fontSize= "16px";
+}
